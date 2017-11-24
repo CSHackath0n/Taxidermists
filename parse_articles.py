00f22034b6abd3dffe2d19cd4e1c2b3eff1ede59
Renaud Jollet De Lorenzo <rjollet@renaud.laptop>
2017-11-24 13:08:18 +0100

    Parse article from file or url
    parse_articles.py contains 2 functions you need
    parse_articles_from_file with file path and line separator(optional)
    parse_articles_from_url with url and line separator(optional)

---
+++ parse_articles.py
@@ -0,0 +1,46 @@
+import requests
+
+def download_articles(url):
+    r = requests.get(url)
+    return r.text
+
+def parse_article(article_txt, line_sep):
+    parts = article_txt.strip().split(line_sep)
+    article_dict = {
+        part.split(':',1)[0].replace('####','').strip(): part.split(':',1)[1].strip()
+        for part in parts
+    }
+    return format_article_dict(article_dict)
+
+def format_article_dict(article_dict):
+    article_dict['seqNo'] = int(article_dict['seqNo'])
+    article_dict['topics'] = [
+        t.strip()
+        for t in article_dict['topics'].replace('"','').split(',')
+    ]
+    article_dict['topicsDescription'] = [
+        t.strip()
+        for t in article_dict['topicsDescription'].replace('"','').split(',')
+    ]
+    return article_dict
+
+def parse_articles(articles_txt, line_sep):
+    articles_txt_list = articles_txt.split(line_sep + line_sep)
+    articles_dict_list = [
+        parse_article(article_txt, line_sep=line_sep)
+        for article_txt in articles_txt_list
+    ]
+    return articles_dict_list
+
+def parse_articles_from_url(url, line_sep='\r\n'):
+    articles_txt = download_articles(url)
+    return parse_articles(articles_txt, line_sep)
+
+def parse_articles_from_file(file_path, line_sep='\n'):
+    with open(file_path, 'r') as f:
+        articles_txt = f.read()
+    return parse_articles(articles_txt, line_sep)
+
+
+# print(parse_articles_from_files('Data/News300.txt'))
+# print(parse_articles_from_url('https://hackathon17.mope.ml/HackathonSite/News300.txt'))
